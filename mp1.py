c7c348620196ae3aca1110a3779e8a67c8c0378b
Rohan Miguel Zabala <72007686+S-kurge@users.noreply.github.com>
2024-11-07 22:42:17 +0800

    Update mp1.py
    Start implementing the basic functions.

--- mp1.py
+++ mp1.py
@@ -1,1 +1,34 @@
+import sys
+import time
+
+# Constants such as the points and the emojis
+WALL = "🧱"       
+EGG = "🥚"       
+GRASS = "🟩"      
+NEST_EMPTY = "🪹" 
+NEST_WITH_EGGS = "🪺" 
+PAN = "🍳" 
+NEST_POINT = 10
+PAN_POINT = 5
+
+
+def load_level(): 
+    if len(sys.argv) < 2:
+        print('The game requires a filename to start.', file=sys.stderr)
+        return
+    else:
+        with open(sys.argv[1], encoding='utf-8') as f:
+        	rows_num = int(f.readline().strip())
+        	moves_num = int(f.readline().strip())
+        	grid = [(f.readline().strip()) for row in range(rows_num)]
+            main_game(grid, moves_num)
+
+
+def main_game():
+
+
+
+def tilt():
+
+load_level()
 
